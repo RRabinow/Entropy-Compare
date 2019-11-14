f03fd95d7c11e367df1bfd79b0277dd3de24bbb3
RRabinow <ryan.l.rabin@gmail.com>
2019-11-14 14:21:10 -0700

    Fixed string to byte, removed print() feedback

--- Entropy_Compare.py
+++ Entropy_Compare.py
@@ -13,17 +13,15 @@ def computeShannonEntropy(inputString):
 	frequencies = []
 	uniqueValues = []
 	for i in range(0, len(inputString)):
-		values.append("0" * (7-len(format(inputString[i:i+1]))) + format(inputString[i:i+1])) #If bit string length is less than 7, pad the beginning with 0's
-		print(values[i])
-	for i in values:
+		values.append("0" * (7-len(bin(int.from_bytes(inputString[i:i+1].encode(), 'big'))[2:])) + bin(int.from_bytes(inputString[i:i+1].encode(), 'big'))[2:]) #If bit string length is less than 7, pad the beginning with 0's
+	for i in values: #Transform a list into a 'set'
 		if i not in uniqueValues:
-			uniqueValues.append(i)
-	for i in range(0, len(uniqueValues)):
+			uniqueValues.append(i) #really just a list with unique elements
+	for i in range(0, len(uniqueValues)): #for each unique value, count its occurance
 		frequencies.append(values.count(uniqueValues[i]))
-		total += values.count(uniqueValues[i])
-	for i in range(0, len(frequencies)):
-		frequencies[i] = math.fabs((frequencies[i]/total)*math.log(frequencies[i]/total,2)) #Shannon entropy -pilog2(pi)
-		entropy += frequencies[i]
+		total += values.count(uniqueValues[i]) #keep a running total, could just use len(), but we already have to have this loop
+	for i in range(0, len(frequencies)): 
+		entropy += math.fabs((frequencies[i]/total)*math.log(frequencies[i]/total,2)) #Shannon entropy -pilog2(pi)
 	return entropy
 
 x, y = getUserInput()
